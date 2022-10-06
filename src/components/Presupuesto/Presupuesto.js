bbfef6fd46990096cbcfbec6640edfec2279ef3e
leandro goñi <leandrodavid13@hotmail.com>
2022-10-06 14:11:04 -0300

    form

--- src/components/Presupuesto/Presupuesto.js
+++ src/components/Presupuesto/Presupuesto.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Link } from "react-router-dom";
 // scss
 import './Presupuesto.scss';
-
+// icon
+import AddIcon from '@mui/icons-material/Add';
 function Presupuesto() {
 
     return (
@@ -13,22 +14,46 @@ function Presupuesto() {
             <div className='PresupuestoCreacion'>
                 <div className='PresupuestoCreacionMateriales'>
                     <p>MATERIALES</p>
-                    <div className='ListadoMateriales'>
-                        <div className='AgregarMaterial'>
-                            <div>
-                                <label for="material">MATERIAL</label>
-	                            <input type="text" name="material" id="material" placeholder="MATERIAL"/>
-                            </div>
-                            <div>
-                                <label for="material">PRECIO</label>
-	                            <input type="text" name="precio" id="precio" placeholder="PRECIO"/>
-                            </div>
-                            <button>+</button>
+                    <div className='AddListadoMateriales'>
+                        <div>
+                            <form className='AgregarMaterial'>
+                                <div className='AddMaterial'>
+                                    <input type="text" id='material' placeholder="Agregar Material..."/>
+                                </div>
+                                <div className='AddPrecio'>
+                                    <input type="text" id='material' placeholder="Precio..."/>
+                                </div>
+                                <button className='ButtonAdd' id='AddListado'>
+                                    <AddIcon
+                                        className='AddIcon'
+                                    />
+                                </button>
+                            </form>
                         </div>
+                    </div>
+                    <div className='ListadoMateriales'>
+
                     </div>
                 </div>
                 <div className='PresupuestoCreacionTiempo'>
                     <p>TIEMPO ESTIMADO</p>
+                    <div className='ListadoMateriales'>
+                        <div>
+                            <form>
+                                <div className='AddTiempoTotal'>
+                                    <input type="text" id='tiempoTotal' placeholder="Tiempo total..."/>
+                                </div>
+                                <div className='AddTiempoHoras'>
+                                    <input type="text" id='tiempoHoras' placeholder="horas..."/>
+                                </div>
+                                <button className='ButtonAdd' id='AddTiempo'>
+                                    <AddIcon
+                                        className='AddIcon'
+                                    />
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='PresupuestoFooter'>
